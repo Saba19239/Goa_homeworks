a2984fbfc0fa8c716a9efe1662a788e73047afde
Saba19239 <saba.batiashvili01@gmail.com>
2026-02-27 00:40:30 +0400

    commit

---
+++ level 3/homework/hw.py
@@ -0,0 +1,102 @@
+# 2) კომენტარების სახით ახსენით რა არის "კომენტარი", როგორ უნდა შევქმნათ და რა დანიშულება გააჩნია.
+
+# 3) კომენტარების სახით ახსენით რა არის case-sensitive და case-insensetive პროგრამირების ენები.
+
+# 4) კომენტარებით ახსენით რა არის snake_case-ი და შექმენით მისი გამოყენებით რამდენიმე ცვლადი 
+# შესაბამისი მნიშვნელობებით. შეეცადეთ ააწყოთ ასეთი ცვლადებით ერთი დიდი წინადადება >3
+
+# 5) კომენტარებით ახსენით რა არის bug და debugging-ი.
+
+# 6) გამოასწორეთ შეცდომები შემდეგ კოდში:
+
+# 1. name = "Lika"
+#    print(namme)
+
+# 2. number = 5
+#    text = " apples"
+#    result = number + text
+#    print(result)
+
+# 3. name = liKa
+#    name2 = naame + "4"
+
+# 4. first_user = "Lika"
+#    2nduser = "Giorgi"
+#    print(first user)
+
+# 5. first name = Data
+#    last % name = "random"
+
+# 6. first_user = "Lika"
+#    2nduser = "Giorgi"
+#    print(first user)
+
+# პასუხები:
+
+# 2) "კომენტარი", არის სიტყვებთ წარმოდგენილი, მას უწერია "#" წინ, და ის არის ადამიანებისთვის 
+# კომუნიკაციისთვის, კომპიუტერები მას ვერ კითხულობენ
+
+# 3) case-sensetive პროგრამულული ენა, არის ის ენა, რომელშიც უნდა დავიცვათ დიდი და პატარა ასოები,
+#  ასეთი ენაა პითონი, case-insensitive პროგრამული ენა, არის ენა, რომელშიც არ არის საჭირო რომ 
+# დავიცვათ დიდი და პატარა ასოები
+
+# 4) snake_case არის გამოყენებული ცვლადების სახელებისთვის, რადგან ცვლადის სახელს, არ შეიძლება რომ
+#  ცარიელი ადგილი ჰონდეს, ქვედა ტირე ასრულებს ამას, ასევე არ შეიძლება რომ სახელში დიდი ასო შედიოდეს
+
+first_name = "saba"
+last_name = "batiashvili"
+favourite_color = "orange"
+
+print( first_name, last_name, favourite_color )
+
+# 5) სიტყვა "bug" ნიშნავს რაღაც შეცდომას კოდში, მაგალითად syntax-ის დარღვევა. debugging არის ერთ-ერთი კომპიუტერული პროცესი, რის დროსაც ჩვენ ანდ სისტემა, 
+# პოულობს შეცდომებს, და ასწორებს მათ
+
+# 6)
+
+# 1. name = "Lika"
+#    print(namme)
+
+# name = "Lika"
+# print(name)
+
+# 2. number = 5
+#    text = " apples"
+#    result = number + text
+#    print(result)
+
+# number = 5
+# text = "apples"
+# result = number , text
+# print(result)
+
+# 3. name = liKa
+#    name2 = naame + "4"
+
+# name = lika
+# name2 = name + 4
+
+# 4. first_user = "Lika"
+#    2nduser = "Giorgi"
+#    print(first user)
+
+# first_user = "Lika"
+# 2nduser = "Giorgi"
+# print(first_user)
+
+# 5. first name = Data
+#    last % name = "random"
+
+# 
+
+# 6. first_user = "Lika"
+#    2nduser = "Giorgi"
+#    print(first user)
+
+# first_user = "Lika"
+# 2nduser = "Giorgi"
+# print(first_user)
+
+
+
+
