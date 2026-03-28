e12dbbd4830451b906558765744c2af2fdd83e5c
Saba19239 <saba.batiashvili01@gmail.com>
2026-03-28 21:15:40 +0400

    commit

---
+++ level 8/Classwork/iomchydtcry.py
@@ -0,0 +1,60 @@
+# 1) კომენტარების სახით ახსენით რა არის logical operators, ჩამოწერეთ თითოეული და განმარტეთ რომელი რა დროს რა შედეგს გვიბრუნებს.
+
+# 2) გააკეთეთ ყველა შედარებით ოპერატორებზე 3 მაგალითი.
+
+# 3) გააკეთეთ 2 მაგალითი logical operator-ებზე.
+
+
+#---------------
+#1)
+
+# Logical operators გამოიყენება სიმართლეს (True) და ტყუილს (False) გადასამოწმებლად.
+
+# 1) and, აბრუნებს True თუ ორივე პირობა True
+# 2) or,    აბრუნებს True თუ მინიმუმ ერთი პირობა True
+
+#-----------------
+#2)
+
+# 1) ==
+print(5 == 5)
+print(3 == 7)
+
+# 2) !=
+print(4 != 5)
+print(8 != 8)
+
+# 3) 
+# >  
+print(10 > 7)   
+print(5 > 8)
+print(4 > 2)
+#<
+print(3 < 2)    
+print(8 < 2)
+print(9 < 7)
+#>= 
+print(5 >= 5)  
+print(3 >= 7)
+print(1 >= 5)
+#<=
+print(6 <= 4)   
+print(5 <= 2) 
+print(6 <= 3)
+
+#---------
+#3)
+
+# 1) and
+print(True and True)    
+print(True and False)
+
+print(True or False)
+print(True or False)
+
+
+
+
+
+
+
