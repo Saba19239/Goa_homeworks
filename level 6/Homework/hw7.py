f1c7bc92f5a5fd6d5cb4440936306b5fe147268b
Saba19239 <saba.batiashvili01@gmail.com>
2026-03-29 19:18:26 +0400

    commit

--- level 6/Homework/hw7.py
+++ level 6/Homework/hw7.py
@@ -1,3 +1,5 @@
+# 7) მომხმარებელს შემოატანინეთ 5 რიცხვი სხვადასხვა დამოუკიდებელი input-ების სახით, თქვენი დავალებაა დაბეჭდოთ მათი საშუალო არითმეტიკული.
+
 num_11 = float(input("insert number here: "))
 
 num_12 = float(input("insert number here: "))
@@ -8,6 +10,6 @@ num_14 = float(input("insert number here: "))
 
 num_15 = float(input("insert number here: "))
 
-avg = (num_11 + num_12 + num_13 + num_14 + num_15)
+avg = ((num_11 + num_12 + num_13 + num_14 + num_15)/5)
 
-print(avg)
+print(int(avg))
