8f083a22515853303a26ffefc792c5c3e0a9f3b3
Saba19239 <saba.batiashvili01@gmail.com>
2026-06-06 21:55:59 +0400

    commit

---
+++ level 18/Classwork/tham.py
@@ -0,0 +1,31 @@
+# 1)
+numbers = [10, 20, 30, 40, 50, 60, 70]
+
+first_three = numbers[:3]
+print(first_three)
+
+
+
+
+# 2)
+text = "Hello, World!"
+
+word = text[7:12]
+print(word)
+
+
+# 3) 
+colors = ["წითელი", "მწვანე", "ლურჯი", "ყვითელი", "შავი"]
+
+colorebiiii = colors[-2:]
+print(colorebiiii)
+
+
+
+
+# 4) ტექსტის უკუღმა შემოტრიალება
+word = "Python"
+
+reversed_word = word[::-1]
+print(reversed_word)
+
