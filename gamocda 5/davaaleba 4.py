cd2e8c9b3e0bd5202a2fb34692ea92c60d9d0bad
Saba19239 <saba.batiashvili01@gmail.com>
2026-07-26 18:56:54 +0400

    commit

---
+++ gamocda 5/davaaleba 4.py
@@ -0,0 +1,49 @@
+# 4) გააკეთეთ თითო მაგალითი .upper(); .lower(); .capitalize(); .find(), .count(), len(), .endswith(), .startswith() ებზე
+
+# 4)
+# .upper()
+text = "hello world"
+
+print(text.upper())
+
+# .lower()
+
+word = "GOA"
+
+print(word.lower())
+
+# .capitalize()
+
+animal = "dog"
+
+print(animal.capitalize())
+
+# .find()
+
+name = "mate"
+
+print(name.find('a'))
+
+# .count
+
+num = "eeeeee"
+
+print(num.count("e"))
+
+# .len
+
+leangth = "pyhton"
+
+print(len(leangth))
+
+# .endswitch
+
+letter = "&^()^$$*^"
+
+print(letter.endswith("()"))
+
+# .startswith
+
+letter
+
+print(letter.startswith("&"))
---
+++ gamocda 5/davaleba 7.py
@@ -0,0 +1,21 @@
+# 7)შექმენით ფუნქციები რომლებიც გააკეთებენ +, -, *
+
+def plus(a, b):
+    print(a + b)
+
+def minus(a, b):
+    print(a - b)
+
+def devide(a, b):
+    print(a / b)
+
+def multiply(a, b):
+    print(a * b)
+
+plus(8, 9)
+
+minus(4, 2)
+
+devide(9, 3)
+
+multiply(3, 5)
